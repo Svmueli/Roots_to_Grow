4e84a3070e63b097b00e965f1644927e5d9078e5
warrenshiv <warrenshiv@gmail.com>
2024-04-05 23:30:46 +0300

    Add rectangle image and click event to Play Game Page

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -9,6 +9,14 @@ const PlayGame = () => {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold">Play Game Page</h1>
       <p>Selected Character: {character}</p>
+      <div className="mt-8">
+        <div className="border-4 border-gray-800 p-20">
+          <div
+            className="h-64 w-96 rounded-md bg-[url('https://example.com/baobab-tree.jpg')] bg-cover bg-center cursor-pointer"
+            onClick={() => alert('Rectangle clicked!')}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 };
