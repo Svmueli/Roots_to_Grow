3fc8d67f2127540596254d3b1501edfcf9a29a67
warrenshiv <warrenshiv@gmail.com>
2024-04-05 22:50:50 +0300

    Refactor Welcome page to use a modal for character selection

---
+++ src/pages/Play/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Welcome() {
+    return (
+        <div className="flex flex-col justify-center items-center min-h-screen">
+            <h1 className="text-3xl font-bold text-green-700 mb-4">Welcome to the Roots to Grow Game 🎮</h1>
+        </div>
+    );
+}
