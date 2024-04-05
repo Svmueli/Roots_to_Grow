afde6ceb157a613e8b47c8227ece2538da704b46
warrenshiv <warrenshiv@gmail.com>
2024-04-05 23:51:43 +0300

    Refactor PlayGame component and add background image

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import baobabTreeImage from "../../assets/images/bao.webp";
 
 const PlayGame = () => {
   const location = useLocation();
-  const { character } = location.state || { character: 'No character selected' };
+  const { character } = location.state || {
+    character: "No character selected",
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">Play Game Page</h1>
-      <p>Selected Character: {character}</p>
+      <h1 className="text-4xl font-bold text-gray-800 mb-4 mt-2 shadow-lg p-3 rounded-lg bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-white flex justify-center items-center">
+        🎮 Roots To Grow 🕹️
+      </h1>
+      <p className="text-xl font-medium text-gray-700 mb-2 bg-yellow-100 p-2 rounded shadow flex items-center">
+        🧑‍🚀 Selected Character:{" "}
+        <span className="font-bold text-gray-800 ml-2">{character} 🌟</span>
+      </p>
+
       <div className="mt-8">
-        <div className="border-4 border-gray-800 p-20">
-          <div
-            className="h-64 w-96 rounded-md bg-[url('https://example.com/baobab-tree.jpg')] bg-cover bg-center cursor-pointer"
-            onClick={() => alert('Rectangle clicked!')}
-          ></div>
+        {/* Outer div with background image and significantly increased size using inline styles */}
+        <div
+          style={{
+            backgroundImage: `url(${baobabTreeImage})`,
+            height: "40rem", // Adjust the height as needed
+            width: "60rem", // Adjust the width as needed
+          }}
+          className="border-4 border-gray-800 p-20 rounded-md bg-cover bg-center cursor-pointer"
+          onClick={() => alert("Rectangle clicked!")}
+        >
+          {/* Inner div for potential content */}
+          <div className="h-full w-full flex justify-center items-center">
+            {/* Content goes here */}
+          </div>
         </div>
       </div>
     </div>
